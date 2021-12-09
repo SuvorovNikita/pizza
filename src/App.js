8cf265c773339bbadc9c0cb1af109d6703b7cc52
Nikita <nikita@platform500.com>
2021-12-10 01:21:45 +0300

    sort

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Route } from 'react-router';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
-        <Route path="/cart" component={Cart} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
+        <Route path="/cart" render={() => <Cart />} exact />
       </div>
     </div>
   );
